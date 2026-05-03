033e33dc6a6cd8bdb57f4b9c83629dd7775ab713
ehydo1 <eyadabdelkader550@gmail.com>
2026-05-03 13:11:58 -0700

    1.2

---
+++ Assets/Scripts/GrabbingItems.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandSystem : MonoBehaviour
+{
+    [Header("Hand Anchors")]
+    public Transform leftHand;
+    public Transform rightHand;
+
+    [Header("Layer Settings")]
+    public LayerMask grabMask; 
+
+    private GameObject leftItem;
+    private GameObject rightItem;
+
+    // We change these to accept 'InputAction.CallbackContext'
+    public void OnGrabLeft(InputAction.CallbackContext context) 
+    { 
+        // 100% FIX: Only run if the button was JUST pressed
+        if (context.started) TryAction(true); 
+    }
+
+    public void OnGrabRight(InputAction.CallbackContext context) 
+    { 
+        if (context.started) TryAction(false); 
+    }
+
+    void TryAction(bool isLeft)
+    {
+        if (isLeft && leftItem != null) { Drop(true); return; }
+        if (!isLeft && rightItem != null) { Drop(false); return; }
+
+        PickUp(isLeft);
+    }
+
+    void PickUp(bool isLeft)
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 3f, grabMask))
+        {
+            if (hit.transform.CompareTag("Item"))
+            {
+                Grab(hit.transform.gameObject, isLeft);
+            }
+        }
+    }
+
+    void Grab(GameObject obj, bool isLeft)
+    {
+        Transform hand = isLeft ? leftHand : rightHand;
+        if (isLeft) leftItem = obj; else rightItem = obj;
+
+        obj.transform.SetParent(hand);
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
+        
+        obj.layer = LayerMask.NameToLayer("HeldItem");
+
+        if (obj.GetComponent<Rigidbody>())
+            obj.GetComponent<Rigidbody>().isKinematic = true; 
+    }
+
+    void Drop(bool isLeft)
+    {
+        GameObject item = isLeft ? leftItem : rightItem;
+        if (item == null) return;
+
+        item.transform.SetParent(null);
+        item.layer = LayerMask.NameToLayer("Default");
+
+        if (item.GetComponent<Rigidbody>())
+            item.GetComponent<Rigidbody>().isKinematic = false;
+        
+        if (isLeft) leftItem = null; else rightItem = null;
+    }
+}
